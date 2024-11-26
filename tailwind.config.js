b2fe7b70df2dd8bc19f69e929d3ddeeae33db8a0
Rumsie-Grimshaw <cradleofrumsie@gmail.com>
2024-11-26 21:30:14 +0800

    feat: implimented tailwind setup and initialised HTML boilerplate.

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+      './index.html',
+      './pages/**/*.html',
+      './src/**/*.css',
+      './assets/**/*.js'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
