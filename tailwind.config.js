4ad4a57e268fe0eff5d860d083f8391bcc254a35
Rumsie-Grimshaw <cradleofrumsie@gmail.com>
2024-11-28 11:10:18 +0800

    feat: implimented website basic layout and home page content.

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,26 @@ module.exports = {
       './src/**/*.css',
       './assets/**/*.js'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+          'custom-black-pearl': '#14222F',
+          'custom-iris-blue': '#00ADB5',
+          'custom-quiet-abyss': '#200440',
+          'custom-amarklor-violet': '#4c0999',
+          'custom-lightblue': '#94e5ff',
+      },
+      keyframes: {
+          'open-menu': {
+              '0%' : {transform: 'scaleY(0)'},
+              '80%' : {transform: 'scaleY(1.2)'},
+              '100%' : {transform: 'scaleY(1)'},
+          },
+      },
+      animation: {
+        'open-menu': 'open-menu 0.5s ease-in-out forwards',
+      }
+    },
   },
   plugins: [],
-}
+};
 
